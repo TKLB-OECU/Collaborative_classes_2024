66c6f9266f98622a7ca80bc8f416b0d357c0baf6
Riku-21-073 <gp21a073@oecu.jp>
2024-03-19 19:07:12 +0900

    同様

--- node/src/public/javascripts/pages/updateAcceleration.js
+++ node/src/public/javascripts/pages/updateAcceleration.js
@@ -4,11 +4,11 @@
 function updateAcceleration(data) {
   var { x, y, z } = data;
   document.getElementById('xValue').textContent = x;
-  document.getElementById('yValue').textContent = y;
-  document.getElementById('zValue').textContent = z;
+  // document.getElementById('yValue').textContent = y;
+  // document.getElementById('zValue').textContent = z;
   modelAcceleration.x += x;
-  modelAcceleration.y += y;
-  modelAcceleration.z += z;
+  // modelAcceleration.y += y;
+  // modelAcceleration.z += z;
 }
 
 export { updateAcceleration };
